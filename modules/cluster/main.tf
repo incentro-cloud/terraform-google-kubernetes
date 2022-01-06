8c3c49bab22b0f1f75c400b38788f6dd13075be6
Robin Smorenburg <robin@smorenburg.io>
2022-01-06 11:53:09 +0100

    Add default

--- modules/cluster/main.tf
+++ modules/cluster/main.tf
@@ -70,7 +70,7 @@ resource "google_container_cluster" "cluster" {
   dynamic "monitoring_config" {
     for_each = var.monitoring_config == {} ? [] : [var.monitoring_config]
     content {
-      enable_components = lookup(monitoring_config.value, "enable_components", null)
+      enable_components = lookup(monitoring_config.value, "enable_components", ["SYSTEM_COMPONENTS", "WORKLOADS"])
     }
   }
 
